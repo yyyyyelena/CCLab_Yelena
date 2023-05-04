f0d7c6488c94450a7927b2f7969272c402941d0d
yyyyyelena <qy890@nyu.edu>
2023-05-04 10:34:14 +0800

    may.4th
    :)

--- projectB_Page2/sketch.js
+++ projectB_Page2/sketch.js
@@ -1,35 +1,32 @@
 let cam;
 let img;
 
-let button = document.getElementById('start_capture');
+let button = document.getElementById('captureButton');
 let selectedStamp; // need to give it a value in the very beginning
-let stampFlower1;
-let stampFlower2;
-let stampLeaf1;
-let stampLeaf2;
-let stampEarring1;
-let stampEarring2;
-let stamp=[];
-let stampIndicator = 0
-let scaleForImages = 0.06
-let scaleForImagesX=0.06
 
-let buttonFlower = document.getElementById('Flower1')
-let buttonLeaf1 = document.getElementById('Leaf1')
-let buttonLeaf2 = document.getElementById('Leaf2')
-let buttonEarring1 = document.getElementById('Earring1')
-let buttonEarring2 = document.getElementById('Earring2')
+let stamp=[]; //array that makes loading image easier
+let stampIndicator = 0 //click on the button and change the value of indicator to generate different stamps
+let buttonFlower = document.getElementById('flowerStamp')
+let buttonLeaf1 = document.getElementById('leafStamp1')
+let buttonLeaf2 = document.getElementById('leafStamp2')
+let buttonEarring1 = document.getElementById('earringStamp1')
+let buttonEarring2 = document.getElementById('earringStamp2')
+let buttonEyebrow = document.getElementById('eyebrowStamp')
+let scaleForImagesX = 0.09
+let scaleForImages = 0.09
 
-let stampButton=[]
+let textVisible = false;
+
+let exportButton = document.getElementById('export')
+// let flip = false;
 
 
 function preload(){
-  stampFlower1 = loadImage("gallery/Flower-1.png");
-  // stampFlower2 = loadImage("gallery/flower2.pdf");
-  stampLeaf1= loadImage("gallery/Leaf_1.png");
-  stampLeaf2= loadImage("gallery/Leaf2.png");
-  stampEarring1= loadImage("gallery/Earring1.png");
-  stampEarring2= loadImage("gallery/Earring2.png");
+  for(let i=1;i<7;i++){
+    let filename = "gallery/stamp"+i+".png";
+    stamp.push(loadImage(filename));
+    console.log("yess")
+}
 }
 
 function setup() {
@@ -44,90 +41,46 @@ function setup() {
   cnv.parent('canvasContainer');
 
   cam = createCapture(VIDEO);
-
+  image(cam,0,0,4,3)
   img = createImage(640, 480);
-  // capture.hide();
-  // img.hide();
-  // backgroundTexture();
-
-  // stamp=[stampFlower1,stampLeaf1,stampLeaf2,stampEarring1,stampEarring2]
-  // stampButton=[buttonFlower,buttonLeaf1,buttonLeaf2,buttonEarring1,buttonEarring2]
-  // noLoop();
-
 }
-
-// start_capture.addEventListener('click', Captures);
 
 function draw() {
-  background(0);
+  // background(0);
+  button.addEventListener('click', Captures);
+  buttonFlower.addEventListener('click', flowerButtonPressed);
+  buttonLeaf1.addEventListener('click', leaf1ButtonPressed);
+  buttonLeaf2.addEventListener('click', leaf2ButtonPressed);
+  buttonEarring1.addEventListener('click', earring1ButtonPressed);
+  buttonEarring2.addEventListener('click', earring2ButtonPressed);
+  buttonEyebrow.addEventListener('click', eyebrowButtonPressed);
+
+  drag();
 
 
-
-  // 1. load pixels of video
-  // 2. load pixles of image  
-  // 3. look at every single pixel in the video
-  // 4. based on video pixel, color correspondoing pixel in img
-
-  // 5. then draw image:
-  image(img, 0, 0)
-  // might be this to fill full canvas:
-  // image(img, 0, 0, width, height)
-
-
-}
-
-function mousePressed() {
-  flipImages();
-
-  if(mouseX>width*0.1 &&mouseX<capture.width&&mouseY>height*0.1&&capture.height){
-  push()
-    translate(mouseX,mouseY);
-    scale(scaleForImagesX,scaleForImages);
-    image(stamp[stampIndicator],-stamp[stampIndicator].width/2,-stamp[stampIndicator].height/2);
-  pop()
-  }
+  let generateBtn = document.getElementById("generateBtn");
+  generateBtn.addEventListener("click", generateText);
+  exportButton.addEventListener('click',Export);
 
 }
-
-// function backgroundTexture(){
-//   fill(220,random(10,50));
-//   stroke(220,random(10,50));
-//   strokeWeight(2)
-// for (let i =0; i<num_dots;i++){
-//     let x =random()*width
-//     let y = random()*height
-//     ellipse(x,y,3,0.7)
-//   }
-// for (let i =0; i<num_dots;i++){
-//     let m =random()*width
-//     let n = random()*height
-//     ellipse(m,n,0.8,02)
-//   }
-//   let x1 = random()*width;
-//   let y1 = random()*height;
-//   let theta=random*2*PI;
-//   let segmentLength = random()*5+2;
-//   let x2 = cos(theta)*segmentLength+x1;
-//   let y2 = sin(theta)*segmentLength+y1;
-//   line(x1,y1,x2,y2)
-
-//   rect(0,0,width,height);
-// }
 
 function Captures(){
-  // loop()
-  img = createImage(capture.width, capture.height);
+  console.log("capture is working")
 
-  capture.loadPixels();
+  // Pixel Manipulation
+  // 1. load pixels of video
+  // 2. load pixles of image  
+  cam.loadPixels();
   img.loadPixels();
 
-  for (let y = 0; y < capture.height; y++) {
-    for (let x = 0; x < capture.width; x++) {
-      let index = (x + y * capture.width) * 4;
+  // 3. look at every single pixel in the video
+  for (let y = 0; y < cam.height; y++) {
+    for (let x = 0; x < cam.width; x++) {
+      let index = (x + y * cam.width) * 4;
 
-      let r = capture.pixels[index + 0];
-      let g = capture.pixels[index + 1];
-      let b = capture.pixels[index + 2];
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
 
       let avg = (r + g + b) / 3;
       let threshold = 0.3; // play with this number
@@ -146,38 +99,35 @@ function Captures(){
       }
     }
   }
-  console.log("this is working xp");
 
+// 4. based on video pixel, color correspondoing pixel in img
   img.updatePixels();
-  push();
-  scale(0.8);
-  image(img, 0,0, window.width*0.7, window.width*0.7 * capture.height / capture.width);
 
-  pop();
-
+  // 5. then draw image:
+  image(img, 0, 0,width, height);
+// startX, startY, image width, image height (the last two have to be the same as the canvas)
 }
 
-function stampsLayout(){
-  // set-ups for the stamps 
-  for(let i=0; i<stamp.length;i++){
+function drag(){
+  flipImages();
+  if(mouseIsPressed && mouseX>0 && mouseX<width && mouseY>0 && mouseY<height){
     push()
-      scale(0.03);
-      image(stamp[i],window.width*0.65/0.03,( window.height*0.2)*(i+0.2)/0.039);//stamp location
-    pop();
-  }
-
-  for(let i=0; i<stamp.length;i++){
-    push()
-      image(stampButton[i],window.width*0.65/0.03+10,( window.height*0.2)*(i+0.2)/0.039);//stamp location
-    pop();
+    translate(mouseX,mouseY);
+    scale(scaleForImagesX,scaleForImages);
+    image(stamp[stampIndicator],-stamp[stampIndicator].width/2,-stamp[stampIndicator].height/2);
+    pop()
   }
 }
+
 
 function flipImages(){
   if (keyCode === LEFT_ARROW){
-    scaleForImagesX =-0.06
+    scaleForImagesX =-0.09
+    console.log(scaleForImagesX)
   } else if (keyCode === RIGHT_ARROW){
-    scaleForImagesX=0.06
+    scaleForImagesX=0.09
+    console.log(scaleForImagesX)
+
   }
 }
 
@@ -192,13 +142,62 @@ function windowResized(){
 }
 
 
-// select button
-let flowerButton = document.getElementById("flowerStamp");
 
 // attach event listener
 function flowerButtonPressed(){
-    console.log("flower button was pressed");
+  stampIndicator = 0
+  console.log(stamp[stampIndicator])
+  // drag();
     // change global variables that defines whats the current stamp
     // that bariable should than be used inside the p5 code
 }
-flowerButton.addEventListener("click", flowerButtonPressed);
+function leaf1ButtonPressed(){
+  stampIndicator = 1
+  // drag();
+}
+function leaf2ButtonPressed(){
+  stampIndicator = 2
+  // drag();
+}
+function earring1ButtonPressed(){
+  stampIndicator = 3
+  // drag();
+}
+function earring2ButtonPressed(){
+  stampIndicator = 4
+  // drag();
+}
+function eyebrowButtonPressed(){
+  stampIndicator = 5
+  // drag();
+}
+
+
+function generateText(){
+  let textContainer = document.getElementById("textContainer");
+  let newText = "<p>This is a creative project inspired by Frida Kahlo s self portraits. </p>"+
+  "<p>The top left corner of the screen is the mirror, you can see your image over there :)</p>"+
+  "<p>When you are ready, click the capture button, and… Voila!! You ve got your picture captured!!</p>"+
+  "<p>Next, click on the stamps, and move your mouse to wherever you want to put these stamps. By hitting the key ←/→, you are able to flip these stamps :)</p>"+
+  "<p>A small introduction on the stamps:</p>"+
+  "<p>The first one (a bunch of flowers), the second earring(hand), two leaves and the eyebrow are all from Self Portrait Dedicated to Dr. Eloesser</p>"+
+  "<p>The first earring (blue) is from Self Portrait with Curly Hair</p>"+
+  "<p>Note that you can recapture by clicking the capture button, but if you have already added stamps on your image, they will be gone;;</p>"+
+  "<p>If you are ready, click the instruction button again, and this page will disappear~ "
+  "<p>Be creative ;) and do Check out Frida Kahlo s artworks!!</p>"
+
+  textContainer.innerHTML = newText;
+
+
+  if (textVisible) {
+    textContainer.style.display = "none";
+    textVisible = false;
+  } else {
+    textContainer.style.display = "block";
+    textVisible = true;
+  }
+}
+
+function Export (){
+  saveCanvas('myCanvas', 'png');
+}
